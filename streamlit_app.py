3defcec5eb3b5a66f6d5a73e7c1edc66a34cd269
ramabirdanalytics <87057985+ramabirdanalytics@users.noreply.github.com>
2022-06-27 02:10:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,11 @@ fruits_selected= streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.i
 streamlit.dataframe(my_fruit_list.loc[fruits_selected])
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
 
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response.json())
@@ -25,5 +29,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Table format from Json
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
+
