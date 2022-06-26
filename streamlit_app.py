8c19af22cc0e7b303ffad9a7e7260be0579e269f
ramabirdanalytics <87057985+ramabirdanalytics@users.noreply.github.com>
2022-06-26 13:04:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,4 @@ streamlit.multiselect("Pick some Fruits: ", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 fruits_selected= streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Avocado','Banana','Grapefruit'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(my_fruit_list.loc[fruits_selected])
