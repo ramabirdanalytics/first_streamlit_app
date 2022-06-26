c850121f16b26bdc41b9a7872eb8c771e4437a0d
ramabirdanalytics <87057985+ramabirdanalytics@users.noreply.github.com>
2022-06-26 13:06:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,4 @@ streamlit.text ("🥣 Upma Pesarat")
 
 streamlit.title("Breakfast Menu")
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick some Fruits: ", list(my_fruit_list.index))
-fruits_selected= streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Avocado','Banana','Grapefruit'])
-streamlit.dataframe(my_fruit_list.loc[fruits_selected])
+streamlit.multiselect("Pick some Fruits: ", list(my_fruit_list.index),['Avocado','Banana','Grapefruit'])
