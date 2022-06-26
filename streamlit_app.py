158efa63c481f7a00e50a377f3661680edde03bc
ramabirdanalytics <87057985+ramabirdanalytics@users.noreply.github.com>
2022-06-26 12:39:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
+import pandas
 
 streamlit.title("Jai Ganesh Om Sai Ram")
 streamlit.header("Breakfast Menu")
 streamlit.text("🍞 Idli & Dosa")
 streamlit.text ("🥣 Upma Pesarat")
+my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
