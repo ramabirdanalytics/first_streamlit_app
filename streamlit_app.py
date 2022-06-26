95ff26a57388e616ac8fa354002fb2d5dbdd93f1
ramabirdanalytics <87057985+ramabirdanalytics@users.noreply.github.com>
2022-06-26 12:33:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title("Jai Ganesh Om Sai Ram")
 streamlit.Header("Breakfast Menu")
-streamlit.text("Idli & Dosa")
-streamlit.text ("Upa Pesarat")
+streamlit.text("🍞 Idli & Dosa")
+streamlit.text ("🥣 Upma Pesarat")
