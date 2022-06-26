85dcaf61417584f63346fcf59fc21b2379b5c431
ramabirdanalytics <87057985+ramabirdanalytics@users.noreply.github.com>
2022-06-27 02:04:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,4 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
