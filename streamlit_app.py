3ef93dcb3f3962279d9aaf471f37cbe5b20bbc9f
ramabirdanalytics <87057985+ramabirdanalytics@users.noreply.github.com>
2022-06-27 17:32:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,20 +17,25 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected= streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Avocado','Banana','Grapefruit'])
 streamlit.dataframe(my_fruit_list.loc[fruits_selected])
 
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+   streamlit.error("Please select fruit to get information")
+else
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response.json())
 
 # Json Normalized
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # Table format from Json
-streamlit.dataframe(fruityvice_normalized)
+
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
